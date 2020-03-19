6fd6e002caaf0ed5a893c44198301daca1f79406
Robin <robinttt333@gmail.com>
2020-03-19 15:28:11 +0530

    LR scheduler added

--- Video/main.py
+++ Video/main.py
@@ -1,25 +1,52 @@
 from dataset import VideoDataset
 from torch.utils.data.dataloader import DataLoader
-import toml
 import config
 from model import Lipreader
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lrScheduler
+import math
 
 
-def trainModel():
-    model = Lipreader()
+def updateLRFunc(epoch):
+    if epoch >= 5:
+        return math.pow(0.5, (epoch - 5 + 1) / float(5))
+    return 1
+
+
+def trainModel(stage, adam, model, scheduler, dataLoader):
     for idx, batch in enumerate(dataLoader):
         target, input = batch[0], batch[1]
         op = model(input.transpose(1, 2))
+        loss = criterion(op, target)
+        adam.zero_grad()
+        loss.backward()
+        adam.step()
+
+    scheduler.step()
 
 
 if __name__ == "__main__":
+    mode = "train"
+    stage = 2
+
     dataset = VideoDataset("train")
     dataLoader = DataLoader(dataset, batch_size=config.data["batchSize"],
                             shuffle=config.data["shuffle"], num_workers=config.data["workers"])
 
-    mode = "train"
+    model = Lipreader()
+    criterion = nn.CrossEntropyLoss()
+    adam = optim.Adam(model.parameters(), lr=3e-4, weight_decay=0.)
+    scheduler = lrScheduler.LambdaLR(adam, lr_lambda=[updateLRFunc])
     if mode == "train":
-        trainModel()
+        model.train()
+        if stage == 2:
+            for param in model.FrontEnd.parameters():
+                param.requires_grad = False
+            for param in model.resnet34.parameters():
+                param.requires_grad = False
+        for epoch in range(10):
+            trainModel(1, adam, model, scheduler, dataLoader)
     elif mode == "val":
         validateModel()
     else:
