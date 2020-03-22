d1eeb43d16a29e47873803b246048a4d38d51afb
Robin <robinttt333@gmail.com>
2020-03-22 18:52:13 +0530

    resnet plus frontend working for audio

--- Audio/model.py
+++ Audio/model.py
@@ -1,9 +1,20 @@
 import torch.nn as nn
+from resnet import resnet18
 
 
 class Lipreader(nn.Module):
     def __init__(self):
         super(Lipreader, self).__init__()
+        self.Fronted1D = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=80, stride=4, padding=38, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(True)
+        )
+        self.resnet18 = resnet18()
 
     def forward(self, x):
+        x = x.view(-1, 1, x.size(1))
+        x = self.Fronted1D(x)
+        x = self.resnet18(x)
+        x = x.view(-1, 29, 256)
         return x
--- Audio/script.py
+++ Audio/script.py
@@ -9,6 +9,7 @@ path = config.data["path"]
 files = glob.glob(os.path.join("../data", '*', '*', '*.mp4'))
 for file in files:
     file, sr = librosa.load(file, sr=16000)
+    file = file[-19456:]
     """The path to save needs to be corrected based on the data"""
     path_to_save = os.curdir
     np.savez(path_to_save, data=file)
